76cf3f6727b31132afb7b494275d565e28095b63
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-25 15:37:31 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -15,7 +15,7 @@ class Author
   end 
   
   def add_post_by_title(title)
-    post = Post.new(title)
+    title = Post.new(title)
     @posts << title 
     title.author = self 
     @@post_count += 1
